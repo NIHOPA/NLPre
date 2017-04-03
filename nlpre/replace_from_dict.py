2b6449241f5bff5e7b1ef57b83161db0905b6e1c
Harry Baker <harry.baker@lexicalintelligence.com>
2017-04-03 13:40:39 -0400

    fixed punctuation issue in mesh replace_from_dict.py. It now passes all tests

--- nlpre/replace_from_dict.py
+++ nlpre/replace_from_dict.py
@@ -28,6 +28,49 @@ class replace_from_dictionary(object):
         items = df["SYNONYM"].str.lower(), df["replace_token"]
         self.X = dict(zip(*items))
 
+    # tokenize splits sentences based on punctuation. This is a problem for phrases that are to be converted to
+    # MeSH terms. recombine iterates through tokens of a sentence to check if if split punctuation belongs to a
+    # MeSH phrase. It can take both a string or a list of tokens.
+    def recombine(self, sent, keywords):
+        sentence = sent
+        stringFlag = False
+        if isinstance(sent, basestring):
+            sentence = sentence.split()
+            stringFlag = True
+
+        splitKeywords = []
+        for words in keywords:
+            split = words.split()
+            splitKeywords.extend(split)
+
+
+
+        pos = 0
+        while pos < len(sentence) - 1:
+            n = 1
+            merge_token = sentence[pos] + sentence[pos + n]
+
+            replace = True
+
+            # loop until the merged tokens fit a keyword exactly, or are no longer substrings of a keyword
+            while merge_token.lower() not in splitKeywords:
+                if any(merge_token.lower() in string for string in keywords):  #merged tokens appear as substring
+                    n += 1
+                    merge_token += sentence[pos + n]
+                    replace = True
+                    continue
+                else:  # the re-merged substrings will not appear in keywords
+                    replace = False
+                    break
+            if replace:
+                sentence[pos] = merge_token
+                del sentence[pos + 1:pos + 1 + n]
+            pos += 1
+            
+        if stringFlag:
+            sentence = ' '.join(sentence)
+        return sentence
+
     def __call__(self, org_doc):
 
         doc = org_doc
@@ -51,9 +94,11 @@ class replace_from_dictionary(object):
 
                 new_word = self.X[word]
                 word_tokens = word.split()
-
+                sent = self.recombine(sent, keywords)
                 # Check if the substring tokens match
-                tokens = sent.lower().split()
+                tokens = sent.lower().split()       #this is problematic because of the lowercasing? check after lunch
+                #tokens = self.recombine(tokens, keywords)    # tokens should be done before for iteration
+
                 mask = contains_sublist(tokens, word_tokens)
                 while any(mask):
                     idx = mask.index(True)
