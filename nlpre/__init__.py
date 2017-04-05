335a3ed6e7bcfcd19510b1d1fabc1ac0d4895367
Harry Baker <harry.baker@lexicalintelligence.com>
2017-04-05 10:14:07 -0400

    add identify_parenthetical_phrases to module init

--- nlpre/__init__.py
+++ nlpre/__init__.py
@@ -6,6 +6,7 @@ from .decaps_text import decaps_text
 from .pos_tokenizer import pos_tokenizer
 from .dedash import dedash
 from .titlecaps import titlecaps
+from .identify_parenthetical_phrases import identify_parenthetical_phrases
 
 __all__ = [
     'replace_phrases',
