6dc44e2a8cedd8f06aeceba7e00ccc48c10f722d
Harry Baker <harry.baker@lexicalintelligence.com>
2017-04-07 15:30:37 -0400

    debug

--- tests/remove_parenthesis_tests.py
+++ tests/remove_parenthesis_tests.py
@@ -7,87 +7,87 @@ class Remove_Parenthesis_Tests():
         self.remove = remove_parenthesis()
 
     def single_parenthesis_pair_test(self):
-        doc = 'hello (hello world1) world2'
-        doc_right = 'hello world2\nhello world1'
+        doc = 'hello (hello world1) world2.'
+        doc_right = 'hello world2 .\nhello world1 .'
         doc_new = self.remove(doc)
 
         assert_equals(doc_right, doc_new)
 
     def single_brackets_pair_test(self):
-        doc = 'hello [world1] world2'
-        doc_right = 'hello world2\nworld1'
+        doc = 'hello [world1] world2.'
+        doc_right = 'hello world2 .\nworld1 .'
         doc_new = self.remove(doc)
 
         assert_equals(doc_right, doc_new)
 
     def single_curly_pair_test(self):
-        doc = 'hello {world1} world2'
-        doc_right = 'hello world2\nworld1'
+        doc = 'hello {world1} world2.'
+        doc_right = 'hello world2 .\nworld1 .'
         doc_new = self.remove(doc)
 
         assert_equals(doc_right, doc_new)
 
     def multiple_brackets_pair_test(self):
-        doc = 'hello [hello [world1] world2] world3'
-        doc_right = 'hello world3\nhello world2\nworld1'
+        doc = 'hello [hello [world1] world2] world3.'
+        doc_right = 'hello world3 .\nhello world2 .\nworld1 .g'
         doc_new = self.remove(doc)
 
         assert_equals(doc_right, doc_new)
 
     def multiple_curly_pair_test(self):
-        doc = 'hello {hello {world1} world2} world3'
-        doc_right = 'hello world3\nhello world2\nworld1'
+        doc = 'hello {hello {world1} world2} world3.'
+        doc_right = 'hello world3 .\nhello world2 .\nworld1 .'
         doc_new = self.remove(doc)
 
         assert_equals(doc_right, doc_new)
 
     def multiple_parenthesis_pair_test(self):
-        doc = 'hello (hello (world1) world2) world3'
-        doc_right = 'hello world3\nhello world2\nworld1'
+        doc = 'hello (hello (world1) world2) world3.'
+        doc_right = 'hello world3 .\nhello world2 .\nworld1 .'
         doc_new = self.remove(doc)
 
         assert_equals(doc_right, doc_new)
 
 
     def multiple_parenthesis_pair_expand_test(self):
-        doc = 'Ad Ba Ca (Da Ed Ff (Ga Ha) In) Jo. Ka Le'
-        doc_right = 'Ad Ba Ca Jo .\nDa Ed Ff In\nGa Ha\nKa Le'
+        doc = 'Ad Ba Ca (Da Ed Ff (Ga Ha) In) Jo. Ka Le.'
+        doc_right = 'Ad Ba Ca Jo .\nDa Ed Ff In .\nGa Ha .\nKa Le .'
         doc_new = self.remove(doc)
 
         assert_equals(doc_right, doc_new)
     
     def multiple_parenthesis_multiple_inner_pair_test(self):
-        doc = 'Ad Ba Ca (Da (Ed Xa) Ff (Ga Ha) In) Jo. Ka Le'
-        doc_right = 'Ad Ba Ca Jo .\nDa Ff In\nEd Xa\nGa Ha\nKa Le'
+        doc = 'Ad Ba Ca (Da (Ed Xa) Ff (Ga Ha) In) Jo. Ka Le.'
+        doc_right = 'Ad Ba Ca Jo .\nDa Ff In .\nEd Xa .\nGa Ha .\nKa Le.'
         doc_new = self.remove(doc)
 
         assert_equals(doc_right, doc_new)
 
     # Code tests are not working at the moment
     def single_parenthesis_test(self):
-        doc = 'hello (world1 (world2) one two'
-        doc_right = 'hello world1 world2 one two'
+        doc = 'hello (world1 (world2) one two.'
+        doc_right = 'hello world1 world2 one two .'
         doc_new = self.remove(doc)
 
         assert_equals(doc_right, doc_new)
 
     def single_bracket_test(self):
-        doc = 'hello [world1 world2'
-        doc_right = 'hello world1 world2'
+        doc = 'hello [world1 world2.'
+        doc_right = 'hello world1 world2 .'
         doc_new = self.remove(doc)
 
         assert_equals(doc_right, doc_new)
 
     def single_curly_test(self):
-        doc = 'hello {world1 world2'
-        doc_right = 'hello world1 world2'
+        doc = 'hello {world1 world2.'
+        doc_right = 'hello world1 world2 .'
         doc_new = self.remove(doc)
 
         assert_equals(doc_right, doc_new)
 
     def unbalanced_parenthesis_test(self):
-        doc = 'hello (((world1) world2) world3'
-        doc_right = 'hello world1 world2 world3'
+        doc = 'hello (((world1) world2) world3.'
+        doc_right = 'hello world1 world2 world3 .'
         doc_new = self.remove(doc)
 
         assert_equals(doc_right, doc_new)
