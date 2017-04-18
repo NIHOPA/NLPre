033d371f789da0e1602408e8b4d2292360d0810a
Harry Baker <harry.baker@lexicalintelligence.com>
2017-04-18 13:33:26 -0400

    DRY Parenthesis nesting grammar

---
+++ nlpre/Grammars/parenthesis_nester.py
@@ -0,0 +1,37 @@
+import pyparsing as pypar
+
+
+class parenthesis_nester(object):
+
+    """
+    Class that recognizes a grammar of nested parenthesis.
+    """
+
+    def __init__(self):
+        nest = pypar.nestedExpr
+        g = pypar.Forward()
+        nestedParens = nest('(', ')')
+        nestedBrackets = nest('[', ']')
+        nestedCurlies = nest('{', '}')
+        nest_grammar = nestedParens | nestedBrackets | nestedCurlies
+
+        parens = "(){}[]"
+        letters = ''.join([x for x in pypar.printables
+                           if x not in parens])
+        word = pypar.Word(letters)
+
+        g = pypar.OneOrMore(word | nest_grammar)
+        self.grammar = g
+
+    def __call__(self, line):
+        try:
+            tokens = self.grammar.parseString(line)
+        except BaseException:
+            return []
+        return tokens
+    '''
+    Args:
+        line: a string
+    Returns:
+        tokens: a parsed object
+    '''
