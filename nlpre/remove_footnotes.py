871d572d63643872a06cff4aee7bed2d1c9894a9
Harry Baker <harry.baker@lexicalintelligence.com>
2017-05-31 14:14:37 -0400

    one method

--- nlpre/remove_footnotes.py
+++ nlpre/remove_footnotes.py
@@ -17,18 +17,10 @@ class Remove_Footnotes:
 
 
         real_word = Word(pyparsing.alphas)
-        #first_punctuation = '.!?:;'
-        second_punctuation = Word('.!?:,;-')
+        second_punctuation = Word('.!?:,;')
         nums = Word(pyparsing.nums)
 
-        #first_case = real_word + first_punctuation + nums + second_punctuation
-        self.escape_case = real_word + '-' + nums
         self.basic_case = real_word + nums
-        #self.first_case = pyparsing.OneOrMore(real_word | first_punctuation | nums | second_punctuation)
-        #self.second_case = pyparsing.OneOrMore(real_word | nums | second_punctuation)
-        #self.foot_case = pyparsing.OneOrMore(real_word | nums | second_punctuation)
-        #self.foot_case = real_word + pyparsing.OneOrMore(
-        #    nums | second_punctuation)
         self.foot_case = real_word + second_punctuation + nums
 
 
@@ -43,22 +35,17 @@ class Remove_Footnotes:
             new_sentence = []
             for token in sentence.split():
                 try:
-                    x = self.escape_case.parseString(token)
-                    new_sentence.append(token)
-                    continue
+                    x = self.basic_case.parseString(token)
+                    if x[0] not in self.english_words:
+                        new_sentence.append(token)
+                        continue
                 except:
                     try:
-                        x = self.basic_case.parseString(token)
-                        if x[0] not in self.english_words:
-                            new_sentence.append(token)
-                            continue
+                        x = self.foot_case.parseString(token)
+                        print
                     except:
-                        try:
-                            x = self.foot_case.parseString(token)
-                            print
-                        except:
-                            new_sentence.append(token)
-                            continue
+                        new_sentence.append(token)
+                        continue
                 new_sentence.append(x[0])
             join_sentence = ' '.join(new_sentence)
             new_doc.append(join_sentence)
