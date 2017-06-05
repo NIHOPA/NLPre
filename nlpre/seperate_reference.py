901ec68741a7899b62f13a1582117f7185f0e832
Harry Baker <harry.baker@lexicalintelligence.com>
2017-06-05 16:25:28 -0400

    refactor code to be more consistent

--- nlpre/seperate_reference.py
+++ nlpre/seperate_reference.py
@@ -1,6 +1,7 @@
 import pattern.en
 import os
 from Grammars import reference_patterns
+import logging
 
 __internal_wordlist = "dictionaries/english_wordlist.txt"
 __local_dir = os.path.dirname(os.path.abspath(__file__))
@@ -29,6 +30,8 @@ class seperate_reference:
             reference_token: boolean, flag to decide to tokenize removed
                 reference content
         '''
+        self.logger = logging.getLogger(__name__)
+
         self.english_words = set()
         with open(_internal_wordlist) as FIN:
             for line in FIN:
@@ -38,34 +41,6 @@ class seperate_reference:
 
         self.reference_pattern = reference_patterns()
 
-        """
-        real_word = Word(pyparsing.alphas)
-        first_punctuation = Word('.!?:,;')
-        second_punctuation = Word('.!?:,;-')
-        space = Word(' ')
-        nums = Word(pyparsing.nums)
-
-        nest = pyparsing.nestedExpr
-        nestedParens = nest('(', ')')
-        nestedBrackets = nest('[', ']')
-        nestedCurlies = nest('{', '}')
-        nest_grammar = nestedParens | nestedBrackets | nestedCurlies
-
-        letter = Word(pyparsing.alphas, exact=1)
-
-        self.dash_word = WordStart() + real_word + Word('-') + nums + WordEnd()
-
-        self.single_number = WordStart() + real_word + nums + WordEnd()
-
-        self.single_number_parens = WordStart() + real_word + \
-            pyparsing.OneOrMore(nums | nest_grammar | space) \
-            + WordEnd()
-
-        self.number_then_punctuation = letter + nums + second_punctuation + \
-            pyparsing.ZeroOrMore(nums | second_punctuation) + WordEnd()
-        self.punctuation_then_number = letter + first_punctuation + nums + \
-            pyparsing.ZeroOrMore(second_punctuation | nums) + WordEnd()
-        """
     def __call__(self, text):
         '''
         call the parser
@@ -90,20 +65,17 @@ class seperate_reference:
                 # up a word, and if they are not found in the dictionary,
                 # it is assumed it is a chemical name and the number is
                 # not pruned.
-                try:
-                    new_tokens = self.single_number_pattern(token)
+
+                new_tokens = self.single_number_pattern(token)
+                if new_tokens:
                     new_sentence.extend(new_tokens)
                     continue
-                except BaseException:
-                    pass
 
                 # check if word is of the form word(4)
-                try:
-                    new_tokens = self.single_number_parens_pattern(token)
+                new_tokens = self.single_number_parens_pattern(token)
+                if new_tokens:
                     new_sentence.extend(new_tokens)
                     continue
-                except BaseException:
-                    pass
 
                 # Check if the word is of the form word.2,3,4
                 new_tokens = self.identify_reference_punctuation_pattern(
@@ -130,31 +102,43 @@ class seperate_reference:
 
     def single_number_pattern(self, token):
         output = []
-        parse_return = self.reference_pattern.single_number.parseString(token)
+        try:
+            parse_return = self.reference_pattern.single_number.\
+                parseString(token)
 
-        if parse_return[0] not in self.english_words:
-            output.append(token)
-        else:
-            word = parse_return[0]
-            reference = parse_return[1]
+            if parse_return[0] not in self.english_words:
+                output.append(token)
+            else:
+                word = parse_return[0]
+                reference = parse_return[1]
 
-            output.append(word)
+                output.append(word)
+                self.logger.info('Removing references %s from token %s' %
+                                 (reference, token))
 
-            if self.reference_token:
-                output.append("REF_" + reference)
+                if self.reference_token:
+                    output.append("REF_" + reference)
+        except BaseException:
+            output = False
+
         return output
 
     def single_number_parens_pattern(self, token):
         output = []
-        parse_return = self.reference_pattern.single_number_parens.\
-            parseString(token)
-        word = parse_return[0]
-        reference = parse_return[1][0]
+        try:
+            parse_return = self.reference_pattern.single_number_parens.\
+                parseString(token)
+            word = parse_return[0]
+            reference = parse_return[1][0]
 
-        output.append(word)
+            output.append(word)
+            self.logger.info('Removing references %s from token %s' %
+                             (reference, token))
 
-        if self.reference_token:
-            output.append("REF_" + reference)
+            if self.reference_token:
+                output.append("REF_" + reference)
+        except BaseException:
+            output = False
 
         return output
 
@@ -168,6 +152,8 @@ class seperate_reference:
             word = token[:index]
             reference = token[index:]
             output.append(word)
+            self.logger.info('Removing references %s from token %s' %
+                             (reference, token))
 
             if self.reference_token:
                 ref_token = "REF_" + reference
